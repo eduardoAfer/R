b29e8c63080d2765d3247ac0cee10919d2d0051b
Eduardo Ferreira <80621444+eduardoAfer@users.noreply.github.com>
2021-09-18 12:24:32 +0100

    code

---
+++ input:output/ex_7.R
@@ -0,0 +1,10 @@
+# Write a R program to find the maximum and the minimum value of a
+# given vector.
+
+
+MinMax <- function(vec){
+  print("original vector")
+  print(vec)
+  print(paste("max value: ", max(vec)))
+  print(paste("min value: ", min(vec)))
+}
