1f5fe89140bbd1c9481ecb72ef15234a6848e57f
Eduardo Ferreira <80621444+eduardoAfer@users.noreply.github.com>
2021-09-18 09:19:31 +0100

    Update input_output.R
    commit change

--- input:output/input_output.R
+++ input:output/input_output.R
@@ -1,3 +1,4 @@
+# R Programming: Take input from the user and display the values, print the version of R installation 
 
 name <- readline(prompt="Input your name: ")
 age <-  readline(prompt="Input your age: ")
