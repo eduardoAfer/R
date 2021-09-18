eb7a0e2308bab7805c096744cf8b91daed158e72
Eduardo Ferreira <80621444+eduardoAfer@users.noreply.github.com>
2021-09-18 09:27:43 +0100

    code

---
+++ input:output/ex_1.R
@@ -0,0 +1,7 @@
+# Write a R program to create a sequence of numbers from 20 to 50 
+# and find the mean of numbers from 20 to 60 and sum of numbers from 51 to 91
+
+print(vec <- c(20:50))
+print(mean_number <- mean(20:60))
+print(sum_all <- sum(51:91))
+
