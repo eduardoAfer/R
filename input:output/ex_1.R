313dca965b917ee78000d4a79a5515f666555093
Eduardo Ferreira <80621444+eduardoAfer@users.noreply.github.com>
2021-09-18 09:29:25 +0100

    Update ex_1.R
    implementations of print functions

--- input:output/ex_1.R
+++ input:output/ex_1.R
@@ -1,7 +1,10 @@
 # Write a R program to create a sequence of numbers from 20 to 50 
 # and find the mean of numbers from 20 to 60 and sum of numbers from 51 to 91
 
+print("Sequence of numbers from 20 to 50:")
 print(vec <- c(20:50))
+print("Mean of numbers from 20 to 60:")
 print(mean_number <- mean(20:60))
+print("Sum of numbers from 51 to 91:")
 print(sum_all <- sum(51:91))
 
